9846e7d2a9aed9bf28c6e859d3b7b3db649a8846
Jose Figueredo <josefigueredo@linuxmint.linuxmint-domain>
2014-11-17 22:14:58 -0430

    Agregado codigo de graficos, no se muestran aun. Boton de borrar por extension no aparece.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,7 @@ Rails.application.config.assets.precompile += %w( bootstrap.css )
 Rails.application.config.assets.precompile += %w( style.css )
 Rails.application.config.assets.precompile += %w( fuzzy.js )
 Rails.application.config.assets.precompile += %w( jquery-2.1.1.min.js )
+Rails.application.config.assets.precompile += %w( google-charts.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
