fb1d0410297feee6d14990d1e0bdbeec1a7967cd
Jose Figueredo <josefigueredo@linuxmint.linuxmint-domain>
2014-11-18 22:25:39 -0430

    Arreglados detalles de diseno, muestra tablas, no muestra googlecharts

--- app/controllers/constants_controller.rb
+++ app/controllers/constants_controller.rb
@@ -1,2 +1,11 @@
 class ConstantsController < ApplicationController
+
+	def constants
+		connection = ActiveRecord::Base.connection();
+		query = "SELECT information_schema_fuzzy.constants2.constant_name,
+ 				information_schema_fuzzy.constants2.value
+				FROM information_schema_fuzzy.constants2";
+		@cons = connection.execute(query);
+	end
+
 end
--- app/controllers/orderings_controller.rb
+++ app/controllers/orderings_controller.rb
@@ -1,4 +1,11 @@
 class OrderingsController < ApplicationController
+
 	def orderings
+		connection = ActiveRecord::Base.connection();
+		query = "SELECT testing.id,
+				information_schema_fuzzy.fuzzy2_tostring(testing.value) AS
+				value_human_readable FROM testing"
+		@data = connection.execute(query);
 	end
+
 end
